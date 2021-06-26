510f11a5be45e5186e5a65e36623cb7640f7adc8
Nicholas Bellman <nbellman95@gmail.com>
2021-06-25 21:10:41 -0400

    Refactored to allow for main().

--- face-detector.py
+++ face-detector.py
@@ -1,4 +1,4 @@
-#----File Information-----------------------------------------------------------
+#----File Information----------------------------------------------------------#
 #   
 #   Project:        Face Detector
 #   Filename:       face-detector.py
@@ -6,32 +6,38 @@
 #   Author Email:   nbellman95@gmail.com
 #   Description:    
 #
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------#
 
-#----Imports--------------------------------------------------------------------
-
+#----Imports-------------------------------------------------------------------#
 import cv2
 import numpy as np
 from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter.constants import CENTER
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------#
 
-#----Constants/Globals----------------------------------------------------------
+#----Constants/Globals---------------------------------------------------------#
 DARK = '#3B3B3B'
 LIGHT = '#FFFFFF'
 
-CV2_RED = (0, 0, 255)
-CV2_GREEN = (0, 255, 0)
-CV2_BLUE = (255, 0, 0)
+CV2_BLK = (0, 0, 0)         # White
+CV2_WHT = (255, 255, 255)   # Black
+
+CV2_BLU = (255, 0, 0)       # Blue
+CV2_GRN = (0, 255, 0)       # Green
+CV2_RED = (0, 0, 255)       # Red
+
+CV2_YLW = (0, 255, 255)     # Yellow
+CV2_CYN = (255, 255, 0)     # Cyan
+CV2_MAG = (255, 0, 255)     # Magenta
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 
                                      'haarcascade_frontalface_default.xml')
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------#
 
-if (__name__ == "__main__"):
-
-    #----GUI Setup----------------------------------------------------------
+#----Functions-----------------------------------------------------------------#
+def main():
+    #----GUI Setup---------------------------------------------------------#
     window = tk.Tk()
     window.title("Face Detector")
     window.resizable(0, 0)
@@ -55,45 +61,46 @@ if (__name__ == "__main__"):
     display_face.grid(row = 1, column = 1)
 
     v_cap = cv2.VideoCapture(0)
-    #-----------------------------------------------------------------------
-
-    #----Functions----------------------------------------------------------
-    def show_frame():
-        ret, frame = v_cap.read()
-        frame = cv2.flip(frame, 1)
-        blank = frame
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        global face_img
-        faces = face_cascade.detectMultiScale(gray, 1.2, 4)
-        for (x, y, w, h) in faces:
-            #cv2.rectangle(frame, (x,y), (x+w, y+h), CV2_GREEN, 2)
-            a, b = (250 - w) // 2, (250 - h) // 2
-            face_img = blank[y-b:y+h+b, x-a:x+w+a]
-        
-        cv2_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(cv2_img)
-        tk_img = ImageTk.PhotoImage(image = img)
-        
-        display_main.image = tk_img
-        display_main.configure(image = tk_img)
-        
-        try:
-            cv2_face = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
-            f_img = Image.fromarray(cv2_face)
-            f_img.resize((240,240))
-            f_tk_img = ImageTk.PhotoImage(image = f_img)
-
-            display_face.image = f_tk_img
-            display_face.configure(image = f_tk_img, anchor = CENTER)
-        except:
-            pass
-        
-        window.after(33, show_frame)
-    #-----------------------------------------------------------------------
-
-
-    show_frame()
+    
+    show_frame(window, v_cap, display_main, display_face)
     window.mainloop()
+    #----------------------------------------------------------------------#
 
+def show_frame(window, v_cap, display_main, display_face):
+    ret, frame = v_cap.read()
+    frame = cv2.flip(frame, 1)
+    blank = frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    global face_img
+    faces = face_cascade.detectMultiScale(gray, 1.2, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), CV2_GRN, 2)
+        a, b = (250 - w) // 2, (250 - h) // 2
+        face_img = blank[y-b:y+h+b, x-a:x+w+a]
+    
+    cv2_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(cv2_img)
+    tk_img = ImageTk.PhotoImage(image = img)
+    
+    display_main.image = tk_img
+    display_main.configure(image = tk_img)
+    
+    try:
+        cv2_face = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
+        f_img = Image.fromarray(cv2_face)
+        f_img.resize((240,240))
+        f_tk_img = ImageTk.PhotoImage(image = f_img)
+
+        display_face.image = f_tk_img
+        display_face.configure(image = f_tk_img, anchor = CENTER)
+    except:
+        pass
+        
+    window.after(1, show_frame, window, v_cap, display_main, display_face)
+
+    #-----------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
 #----End File-------------------------------------------------------------------
